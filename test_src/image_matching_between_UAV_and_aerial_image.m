f0f38bd42fb9ffc657627ccd9956539086da9f43
YoungRok-SON <dudfhr3349@gmail.com>
2022-12-21 00:13:52 +0900

    SLIC and SIFT implementation done.

--- test_src/image_matching_between_UAV_and_aerial_image.m
+++ test_src/image_matching_between_UAV_and_aerial_image.m
@@ -1,7 +1,7 @@
 %% Trial for image matching between aerial image with UAV acquried image.
 close all; clc; clear;
 
-% Parameters initialization
+%% Parameters initialization
 
 % About Map image Loading
 aerial_map_path      = "../../02_map_images/";
@@ -25,25 +25,32 @@ gsd_aerial_map       = 25; %  [cm]
 resize_factor        = gsd_uav_img/gsd_aerial_map;
 target_size_uav_img  = int16([height_image width_image]*resize_factor);
 
-% Orientation matching
+% About Orientation matching
 target_orientation   = -130; % [deg]
 
-% Feature point extraction
-num_big_SIFT_point   = 1000;
-num_devided_area     = 1500;
+% About Feature point extraction
+use_cropped_map           = true; % 안자르고 하려니까 지도 사이즈가 너무 커서 안되겠음 잘라주긴 해야할 듯
+method_feature_extraction = 'SLIC'; % SIFT
+num_strong_SIFT_feature_point = 5000;
+%%% SLIC parameter
+num_devided_area_uav    = 100;
+
+% About Feature matching
+clear troms
+uav_to_map_tform = affine2d(eye(3));
+confidenceValue = 99.9;
+maxNumTrials = 2000;
+
 %% Load images
 
 % Load Aerial Map data
 map_img     = imread(aerial_map_path_name);
 map_img_rgb = map_img(:,:,1:3);
-figure("Name","Map image");
-imshow(map_img_rgb);
 
 % Load UAV Map Data
 uav_img     = imread(uav_img_path_name);
 uav_img_rgb = uav_img(:,:,1:3);
-figure("Name","UAV image");
-imshow(uav_img_rgb);
+%% Image preprocessing
 
 % Generate gray scale img
 map_img_gray = rgb2gray(map_img);
@@ -51,8 +58,6 @@ uav_img_gray = rgb2gray(uav_img);
 
 % Image Resize
 downsampled_uav_img = imresize(uav_img_gray,target_size_uav_img);
-figure("Name","UAV image");
-imshow(downsampled_uav_img);
 
 % Orientation Matching: 일단 손으로 대강 맞추고 나중에 드론으로 할 때에는 드론 헤딩이랑 같이 쓰지 뭐..
 rotated_img = imrotate(downsampled_uav_img, target_orientation, "bilinear");
@@ -61,40 +66,113 @@ figure("Name","UAV image");
 imshow(rotated_img);
 
 % Map Image crop
-cropped_map = imcrop(map_img_gray);
-imshow(cropped_map);
-hold on
-
-% Feature point extraction
-%%% Method 1: Using only SIFT feature points
-feature_points_uav    = detectSIFTFeatures(rotated_img);
-feature_points_aerial = detectSIFTFeatures(cropped_map);
-
-%%%%%% UAV Image
-figure("Name","UAV image");
-imshow(rotated_img);
-hold on
-strong_feature_points_uav = feature_points_uav.selectStrongest(num_strong_SIFT_feature_point);
-plot(strong_feature_points_uav,'ShowOrientation',100)
-
-%%%%%% Aerial Image
+if (use_cropped_map == true)
+    map_for_matching = imcrop(map_img_gray);
+elseif(use_cropped_map == false)
+    map_for_matching = map_img_gray;
+end
 figure("Name","Map image");
-plot(feature_points_aerial,'ShowOrientation',100)
+imshow(map_for_matching);
+hold on
 
 
-%%% MEthod 2: Using SLIC point as feature points
-[label_uav, num_of_label_uav] = superpixels(rotated_img,num_devided_area);
-[label_aerial, num_of_label_aeiral] = superpixels(cropped_map,num_devided_area);
-%%%%%% UAV Image
-Boundary_mask_uav = boundarymask(label_uav);
-imshow(imoverlay(rotated_img,Boundary_mask_uav,'cyan'), 'InitialMagnification',30);
-%%%%%% Aerial Image
-Boundary_mask_aerial = boundarymask(label_aerial);
-imshow(imoverlay(cropped_map,Boundary_mask_aerial,'cyan'), 'InitialMagnification',30);
+%% Feature point extraction
+if (isequal(method_feature_extraction,"SIFT"))
+    %%% Method 1: Using only SIFT feature points
+    feature_points_uav    = detectSIFTFeatures(rotated_img);
+    feature_points_aerial = detectSIFTFeatures(map_for_matching);
+    
+    %%%%%% UAV Image
+    close all;
+    figure("Name","UAV image");
+    imshow(rotated_img);
+    hold on
+    strong_feature_points_uav = feature_points_uav.selectStrongest(num_strong_SIFT_feature_point);
+    plot(strong_feature_points_uav,'ShowOrientation',100)
+    
+    %%%%%% Aerial Image
+    figure("Name","Map image");
+    imshow(map_for_matching);
+    hold on
+    plot(feature_points_aerial,'ShowOrientation',10);
 
-% Descriptor Extraction
+elseif(isequal(method_feature_extraction,"SLIC"))
+    %%% Calculate Parameter for SLIC of aerial map
+    map_size                = size(map_for_matching);
+    uav_img_size            = size(rotated_img);
+    num_devided_area_aerial = int16(num_devided_area_uav *  (max(map_size)/max(uav_img_size))) * 1; % SLIC 개수... 어떻게 해야할지 3은 매직넘버
+    
+    %%% Method 2: Using SLIC point as feature points
+    [label_uav, num_of_label_uav] = superpixels(rotated_img,num_devided_area_uav,'Compactness',1);
+    [label_aerial, num_of_label_aeiral] = superpixels(map_for_matching,num_devided_area_aerial,'Compactness',1);
+    %%%%%% UAV Image
+    figure("Name","UAV SLIC Image");
+    Boundary_mask_uav = boundarymask(label_uav);
+    imshow(imoverlay(rotated_img,Boundary_mask_uav,'cyan'), 'InitialMagnification',30);
+    %%%%%% Aerial Image
+    figure("Name","Map SLIC Image")
+    Boundary_mask_aerial = boundarymask(label_aerial);
+    imshow(imoverlay(map_for_matching,Boundary_mask_aerial,'cyan'), 'InitialMagnification',30);
+    % Extract feature points at the boundary
+    cunt_uav = 1;
+    for i = 1:1:uav_img_size(1)
+        for j = 1:1:uav_img_size(2)
+            if(Boundary_mask_uav(i,j) == true && rotated_img(i,j) ~= 0)
+                slic_points_uav(cunt_uav, :) = [j i];
+                cunt_uav = cunt_uav+1;
+            end
+        end
+    end
+    cunt_aerial = 1;
+    for i = 1:1:map_size(1)
+        for j = 1:1:map_size(2)
+            if(Boundary_mask_aerial(i,j) == true && map_for_matching(i,j) ~= 0)
+                slic_points_aerial(cunt_aerial, :) = [j i];
+                cunt_aerial = cunt_aerial+1;
+            end
+        end
+    end
+    close all;
 
-% Image Matching
+    figure("Name","UAV SLIC Image");
+    imshow(rotated_img);
+    hold on
+    plot(slic_points_uav(:,1), slic_points_uav(:,2),'Marker','+','MarkerSize',5,'LineStyle','none','Color','r');
+
+    figure("Name","Map SLIC Image");
+    imshow(map_for_matching);
+    hold on
+    plot(slic_points_aerial(:,1), slic_points_aerial(:,2),'Marker','+','MarkerSize',5,'LineStyle','none','Color','r');
+    
+    % Descriptor Extraction
+    feature_points_uav = SIFTPoints(slic_points_uav);
+    feature_points_aerial = SIFTPoints(slic_points_aerial);
+end
+
+% Feature extaction
+[descriptors_uav, feature_points_uav] = extractFeatures(rotated_img,feature_points_uav,"Method","SIFT");
+[descriptors_aerial, feature_points_aerial] = extractFeatures(map_for_matching,feature_points_aerial,"Method","SIFT");
+
+
+%% Image Matching
+
+% Find correspondence between I(n-1) and I(n).
+indices_matched = matchFeatures(descriptors_uav, descriptors_aerial, Unique=true);
+% Get matching points
+matched_points_uav = feature_points_uav(indices_matched(:,1),:);
+matched_points_aerial = feature_points_aerial(indices_matched(:,2),:);
+% Estimate the transformation between I(n) and I(n-1).
+[uav_to_map_tform, inliner_mached_points] = estimateGeometricTransform2D( matched_points_uav, matched_points_aerial, ...
+                                        "affine", Confidence=confidenceValue, maxNumTrials=maxNumTrials);
+inlier_points_sensor = matched_points_uav(inliner_mached_points,:);
+inlider_points_map = matched_points_aerial(inliner_mached_points,:);
+figure('Name',"Result of feature point matching"); 
+ax = axes;
+showMatchedFeatures(rotated_img, map_for_matching, matched_points_uav, matched_points_aerial,"montag",Parent=ax);
+% showMatchedFeatures(rotated_img, map_for_matching, inlier_points_sensor, inlider_points_map,"montag",Parent=ax);
+
+title(ax,"Candidate point matches");
+legend(ax,"Matched points 1","Matched points 2");
 
 %% Preprocessing
 
